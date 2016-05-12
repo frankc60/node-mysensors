90e57f6f234dcad3979132370c03ba780750c28f
frankc60 <frankc60@gmail.com>
2016-05-12 17:33:46 +0100

    using Chart.js, set up, tidy up

--- app.js
+++ app.js
@@ -1,96 +1,80 @@
-var   parser = new require('xml2json');
-var  fs = require('fs');
-
-
 //*********************************************
-// app.js
+
+var parser = new require('xml2json');
+var fs = require('fs');
 var express = require('express');  
 var app = express();  
 var server = require('http').createServer(app);  
 var io = require('socket.io')(server);
+var serialport = require("serialport"); 
 
+//********************************************************
+//WEB SERVER WITH express, http
 //Static folder, images, js, etc
+
 app.use("/bower", express.static(__dirname + '/bower_components'));  
 app.use("/public", express.static(__dirname + "/public"));
 
+
+//process requests
 app.get('/', function(req, res,next) {  
 	console.log("Got a GET request for the homepage");
     res.sendFile(__dirname + '/index.html');
 });
 
-var srv = server.listen(8000, function () {
-
-  console.log("Connected");
-
-});  
-
-
-
-
-
-/*
-app.get('/', function (req, res) {
-   console.log("Got a GET request for the homepage");
-   //res.send('Hello GET');
-
-
-
-	fs.readFile(__dirname + '/index.html', function(err, data) {
-	    if (err) {
-	      console.log(err);
-	      res.writeHead(500);
-	      return res.end('Error loading index.html');
-	    }
-	    res.writeHead(200, {'Content-Type': 'text/html'});
-	    res.end(data);
-	  });
-
-})
-*/
-
-
-
-
-	    
-   
-
-
-
-
-
-
-// creating a new websocket to keep the content updated without any AJAX request
-io.sockets.on('connection', function(socket) {
-  console.log(__dirname);
-  // watching the xml file
-  fs.watchFile(__dirname + '/example.xml', function(curr, prev) {
-    // on file change we can read the new xml
-    fs.readFile(__dirname + '/example.xml', function(err, data) {
-      if (err) throw err;
-      // parsing the new xml data and converting them into json file
-      var json = parser.toJson(data);
-      // send the new data to the client
-      socket.volatile.emit('notification', json);
-    });
-  });
-
-
-
+app.get('/test', function(req, res,next) {  
+	console.log("Got a GET request for the TESTER!!!!");
+    res.send("tester !");
 });
 
 
 
+//set up server to listen on port 8000
+var srv = server.listen(8000, function () {
 
-var serialport = require("serialport"); 
+  console.log("Server up and running!");
+
+});  
+//********************************************************
+
+
+//-------------------------------------------------------------------
+// creating a new websocket to keep the content updated without any AJAX request
+io.sockets.on('connection', function(socket) {
+	console.log("io.socket connected!");
+	// watching the xml file
+	fs.watchFile(__dirname + '/example.xml', function(curr, prev) {
+		// on file change we can read the new xml
+		fs.readFile(__dirname + '/example.xml', function(err, data) {
+			if (err) throw err;
+			// parsing the new xml data and converting them into json file
+			var json = parser.toJson(data);
+			// send the new data to the client
+			socket.volatile.emit('notification', json);
+		});
+	});
+
+	socket.on('messageFromClient', function (data) { 
+		socket.emit("notification", "received messge from client: " + data);
+	});
+	
+	socket.on('disconnect', function () { 
+		//WILL BE IGNORED, AS USER HAS GONE!
+		socket.emit("notification", "socket has disconnected!");
+	});
+});
+//-------------------------------------------------------------------
+
+
+
+//===============================================================
+//SERIAL PORT
 var SerialPort = serialport.SerialPort; 
 
 var serialPort = new SerialPort("/dev/ttyACM0", {
   baudrate: 115200,
   parser: serialport.parsers.readline("\n")
 });
-
-
-
 
 serialPort.on('open', showPortOpen);
 serialPort.on('data', sendSerialData);
@@ -105,20 +89,21 @@ function showPortOpen() {
 function sendSerialData(data) {
    	console.log("data: " + data);
 
-rePattern = new RegExp(/read\:.+((\d)|-)+\ss\=(\d+),c\=(\d+),t\=(\d+),pt\=(\d+),l\=(\d+),sg\=(\d+)\:(.+)/);
+	rePattern = new RegExp(/read\:\s+(\d+)-(\d+)-(\d+)+\ss\=(\d+),c\=(\d+),t\=(\d+),pt\=(\d+),l\=(\d+),sg\=(\d+)\:(.+)/);
 
-arrMatches = data.match(rePattern);
+	//read\:\s+(\d+)-(\d+)-(\d+)+\ss\=(\d+),c\=(\d+),t\=(\d+),pt\=(\d+),l\=(\d+),sg\=(\d+)\:(.+)
 
-//((\d);)+read:\s((\d)|-)+\ss\=(\d+),c\=(\d+),t\=(\d+),pt\=(\d+),l\=(\d+),sg\=(\d+)\:(.*)
+	arrMatches = data.match(rePattern);
 
-if(arrMatches) {
+	if(arrMatches) {
+		data2 = "&nbsp;&nbsp;&nbsp; (1:" + arrMatches[1] + ")(2:" +arrMatches[2] + ")(3:" +arrMatches[3] + ") s:" + arrMatches[4] + " c:" + arrMatches[5] + " t:" + arrMatches[6] + " pt:" + arrMatches[7] + " l:" + arrMatches[8] + " sg:" + arrMatches[9] + " data:" + arrMatches[10];
+	} else {
+		data2 = " [ignored]";
+	}
 
-	data2 = " AA:" + arrMatches[3] + " BB:" + arrMatches[4] + " CC:" + arrMatches[5] + " DD:" + arrMatches[6] + " EE:" + arrMatches[7] + " FF:" + arrMatches[8] + " GG:" + arrMatches[9];
-} else {
-	data2 = "<blank>";
-}
+	io.emit('update', data + data2);
 
-	io.sockets.emit('update', data + data2);
+
 }
  
 function showPortClose() {
@@ -128,3 +113,4 @@ function showPortClose() {
 function showError(error) {
    console.log('Serial port error: ' + error);
 }
+//===============================================================
